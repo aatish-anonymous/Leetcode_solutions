d3a29d09d6c8d4d972fd02e55bdb9fbc43f3285c
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-06-15 13:23:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} arr
+ * @returns {void}
+ */
+
+//[12,2,34,10,8]
+class Solution {
+    reverseArray(arr) {
+        // code here
+        
+        let left =  0;
+        let right = arr.length-1;
+        
+        while(left < right){
+            let temp  =  arr[left]
+            arr[left] =  arr[right];
+            arr[right] = temp ;
+
+               left ++;
+               right --;
+        }
+        
+        // let ulta = arr.reverse();
+        // return ulta ;
+        
+    //   let reverse = [];
+    //   for(let i=0; i<arr.length; i++){
+    //       reverse.push(arr[arr.length-1-i]);
+    //   }
+    //   return reverse ;
+    
+    }
+}
+
+
