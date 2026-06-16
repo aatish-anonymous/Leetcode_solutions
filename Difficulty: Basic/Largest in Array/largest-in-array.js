ea459e24cd05a63f1cdd723d1216f37d6dea671a
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-06-16 12:24:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.js
@@ -0,0 +1,21 @@
+
+class Solution {
+    /**
+    * @param number[] arr
+
+    * @returns number
+    */
+    largest(arr){
+        let larger = arr[0];
+        for(let i=1; i<=arr.length; i++){
+            if(arr[i]>larger){
+                larger = arr[i]
+            }
+        }
+        return larger
+    }
+    
+}
+  
+  
+  
