76f653588164f6df0b559e31a68af2a5df807814
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-06-20 11:12:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.js
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.js
@@ -7,16 +7,36 @@ class Solution {
     twoSum(arr, target) {
         // code here
         
-        let seen = new Set();
-        for(let num of arr){
-            let complement  = target - num;
-            
-            if(seen.has(complement)){
+        // current value +  need value? = target, we can short out above problem like this 
+        // target - current value  = need value we find right  ?
+        
+        let memory  = {}
+        
+        for(let i=0; i<arr.length; i++){
+            let neededValue = target - arr[i];
+            if(memory[neededValue] !== undefined){
                 return true;
+                // return [memory[neededValue], i];
             }
-            seen.add(num);
+            
+            memory[arr[i]] = true;
+            
+            // memory[arr[i]] = i
+            // memory.set(arr[i],i);
         }
-        return false;
+        
+          return false 
+        
+        // let seen = new Set();
+        // for(let num of arr){
+        //     let complement  = target - num;
+            
+        //     if(seen.has(complement)){
+        //         return true;
+        //     }
+        //     seen.add(num);
+        // }
+        // return false;
         
 //  for (let i = 0; i < arr.length; i++) {
 //         for (let j = i + 1; j < arr.length; j++) {
