760f43b70d8ed8fb8df684abfe3e976bd76362c2
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-06-25 13:28:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bitonic Point/bitonic-point.js
+++ Difficulty: Easy/Bitonic Point/bitonic-point.js
@@ -1,5 +1,3 @@
-// User function Template for javascript
-
 /**
  * @param {number[]} arr
  * @returns {number}
@@ -9,14 +7,34 @@ class Solution {
 
     findMaximum(arr) {
         // your code here
-       let max = arr[0];
-       
-       for(let i=1; i<arr.length; i++){
-           if(arr[i]>max){
-               max = arr[i];
-       }
-      }
-      return max;
+         let low = 0 ;
+         let high = arr.length-1; 
+         
+         //condition 
+         while(low<high){
+             
+             // find the mid element 
+             let mid  =  Math.floor((low+high)/2);
+             
+             //contion for prevent breaking our code like means what-if our mid is less than 0
+             
+             let left  = (mid>0) ? arr[mid-1] : -Infinity;
+             let right  = (mid < arr.length-1) ? arr[mid+1] : -Infinity;
+             
+             //if our mid is greater than left and right means element we found
+            if(arr[mid]>left && arr[mid]>right){
+                return arr[mid];
+            }
+            
+            //move to increasing side 
+            else if (arr[mid] < right ){
+                low = mid + 1 ;
+            }else{
+                high= mid-1
+            }
+             
+         }
+         
+         return arr[low] ;
     }
-    
 }
