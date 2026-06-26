48dffc1dfe598e0ead1324f071d9607624340a83
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-06-26 16:05:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count the Zeros/count-the-zeros.js
+++ Difficulty: Easy/Count the Zeros/count-the-zeros.js
@@ -1,17 +1,12 @@
-// User function Template for javascript
-
 class Solution {
     countZeroes(arr) {
-        // write your code here
-        
-    let count = 0;
-        
-        for(let i=0; i<arr.length; i++){
-            if(arr[i]===0) {
-                count++
-            }
-    
-        }
-        return count;
+        // code here
+        let count  = 0;
+       for(let i=0; i<arr.length ; i++){
+           if(arr[i]===0) {
+               count++;
+           }
+       }
+       return count ;
     }
 }
