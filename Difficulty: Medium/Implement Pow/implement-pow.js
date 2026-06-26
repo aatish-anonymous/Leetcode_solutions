5d81e517b045f7bb257e8507087e8cf4cd4ed1e4
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-06-26 13:30:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Pow/implement-pow.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * Computes b raised to the power e recursively.
+     * @param {number} b - The base.
+     * @param {number} e - The exponent.
+     * @returns {number} - The result of b^e.
+     */
+    power(b, e) {
+        // Code here
+        return Math.pow(b,e);
+    }
+}
