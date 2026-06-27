05067011e9956a59bf4681fd513d073527d615e0
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-06-27 11:26:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Check for Binary String/check-for-binary-string.js
+++ Difficulty: Basic/Check for Binary String/check-for-binary-string.js
@@ -1,13 +1,8 @@
-/**
- * @param {string} s
- * @returns {boolean}
- */
-
 class Solution {
     isBinary(s) {
         // code here
-        for(let i=0; i<s.length; i++){
-            if(s[i] !== '0' && s[i] !== '1'){
+        for(let ch of s){
+            if(ch !== '0' && ch !== '1'){
                 return false;
             }
         }
