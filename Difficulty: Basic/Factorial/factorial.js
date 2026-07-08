9cb24289cf4df0b27e966e19393d0934ca412708
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-07-08 12:24:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.js
+++ Difficulty: Basic/Factorial/factorial.js
@@ -5,19 +5,8 @@
 
 class Solution {
     factorial(n) {
+        if(n===0||n===1) return 1
         // code here
-        if(n===0) return 1
-        if(n===1) return 1
-        
-        let fact  = 1;
-        
-      for(let i=0; i<n; i++){
-          fact = fact*(n-i) ;
-      } 
-      
-      return fact ;
-    //   if(n===0) return 1;
-       
-    //   return n*this.factorial(n-1);
+        return n*this.factorial(n-1);
     }
 }
