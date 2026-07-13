066cc99f3e4f47a937bc86ef22413afe85bada24
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-07-13 11:34:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.js
+++ Difficulty: Basic/Reverse a String/reverse-a-string.js
@@ -7,9 +7,9 @@ class Solution {
 
     reverseString(s) {
         // code here
-        let reverse = '';
+        let reverse = " ";
         for(let i=s.length-1; i>=0; i--){
-            reverse += s[i]
+             reverse += s[i];
         }
         return reverse;
     }
