b87e729e8405bfd055a509fd036be4c822221549
Devang Kartik <93068034+aatish-anonymous@users.noreply.github.com>
2026-07-22 16:25:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @returns {boolean}
+ */
+class Solution {
+    isPrime(n) {
+        // code here
+        if(n<=1) return false ;
+        
+        for(let i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
